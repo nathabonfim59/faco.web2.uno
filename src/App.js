625c72eeeeb3b39c2066a2373b56467aad1514eb
nathanael-winsite <nathanael@winsitesistemas.com.br>
2024-10-04 20:24:28 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Contador from './components/Contador';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
