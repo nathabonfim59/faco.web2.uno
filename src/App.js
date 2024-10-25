844aa81f1dc1cfaf748ee6e122456a8af332dd88
nathanael-winsite <nathanael@winsitesistemas.com.br>
2024-10-25 20:01:34 -0300

    feat: estilizacao de cartas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Card from './components/Card';
 function App() {
   return (
     <>
-      <Card />
+      <Card nome="1" />
+      <Card nome="2" />
+      <Card nome="A" />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import "./card.css"
+import React from "react";
+import "./card.css";
 
-const Cabecalho = () => { };
-const Centro = () => { };
-const Rodape = () => { };
+const Cabecalho = (props) => {
+  return (
+    <div className="cabecalho">
+      <span>{props.nome}</span>
+    </div>
+  );
+};
 
-const Card = () => {
-    return (
-        <div className="carta">
-            <Cabecalho/>
-            <Centro/>
-            <Rodape/>
-        </div>
-    )
-}
+const Centro = (props) => {
+  return (
+    <div className="centro">
+      <span>{props.nome}</span>
+    </div>
+  );
+};
+
+const Rodape = (props) => {
+  return (
+    <div className="rodape">
+      <span>{props.nome}</span>
+    </div>
+  );
+};
+
+const Card = (props) => {
+  // Get color based on first character (R, B, G, Y)
+  const getCardColor = () => {
+    const colors = {
+      'R': '#ff0000',
+      'B': '#0000ff',
+      'G': '#00aa00',
+      'Y': '#ffaa00',
+    };
+    return colors[props.nome[0]] || '#ff0000';
+  };
+
+  const cardStyle = {
+    backgroundColor: getCardColor()
+  };
+
+  return (
+    <div className="carta" style={cardStyle}>
+      <Cabecalho nome={props.nome} />
+      <Centro nome={props.nome} />
+      <Rodape nome={props.nome} />
+    </div>
+  );
+};
 
 export default Card;
